7eaed7ae0c8fb12c93763c27248cb54dd1935225
rhinoxD <coolshiv0721@gmail.com>
2022-02-02 10:41:35 +0530

    Fetching data

---
+++ quote-generator/script.js
@@ -0,0 +1,20 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+  } catch (error) {}
+}
+
+// On Load
+getQuotes();
