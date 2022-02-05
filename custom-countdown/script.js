90b085633740fd5981f3270b3923f54b319ceef6
rhinoxD <coolshiv0721@gmail.com>
2022-02-05 10:01:49 +0530

    Logging countdown title and date

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,6 +2,20 @@ const countdownForm = document.getElementById('countdownForm');
 const inputContainer = document.getElementById('input-container');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+let savedCountdown;
+
 // Set Date Input Min & Value with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+function updateCountdown(e) {
+  e.preventDefault();
+  // Set title and date, save to localStorage
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
