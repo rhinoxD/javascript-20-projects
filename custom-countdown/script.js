84f0b375eae672f19f2a876e680e3fa0cb104d13
rhinoxD <coolshiv0721@gmail.com>
2022-02-05 10:10:42 +0530

    Update countdown functionality added

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,19 +2,52 @@ const countdownForm = document.getElementById('countdownForm');
 const inputContainer = document.getElementById('input-container');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdownTitle = '';
 let countdownDate = '';
-let savedCountdown;
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set Date Input Min & Value with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Populate Countdown / Complete UI
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+  // Populate Countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+  // Hide Input
+  inputContainer.hidden = true;
+  // Show Countdown
+  countdownEl.hidden = false;
+}
 
 function updateCountdown(e) {
   e.preventDefault();
   // Set title and date, save to localStorage
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
+  // Get number version of current Date, updateDOM
+  countdownValue = new Date(countdownDate).getTime();
+  updateDOM();
 }
 
 // Event Listener
