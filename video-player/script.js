5e28de504f4aa5fd2af96c009d8af959ddf8cc37
rhinoxD <coolshiv0721@gmail.com>
2022-02-06 10:35:27 +0530

    Cleanup

--- video-player/script.js
+++ video-player/script.js
@@ -147,11 +147,7 @@ let fullscreen = false;
 
 // Toggle fullscreen
 function toggleFullscreen() {
-  if (!fullscreen) {
-    openFullscreen(player);
-  } else {
-    closeFullscreen();
-  }
+  !fullscreen ? openFullscreen(player) : closeFullscreen();
   fullscreen = !fullscreen;
 }
 
