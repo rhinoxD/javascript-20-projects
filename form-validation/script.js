4f2f56f338a95ef9bf956cbcfaf7870ef76b17e3
rhinoxD <coolshiv0721@gmail.com>
2022-02-06 10:56:08 +0530

    Validate Passwords, Store Data

--- form-validation/script.js
+++ form-validation/script.js
@@ -18,12 +18,51 @@ function validateForm() {
     messageContainer.style.borderColor = 'red';
     return;
   }
+  // Check to see if both password fields match
+  if (password1El.value === password2El.value) {
+    // If they match, set value to true and borders to green
+    passwordsMatch = true;
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+  } else {
+    // If they don't match, border color of input to red, change message
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password2El.style.borderColor = 'red';
+    return;
+  }
+  // If form is valid and passwords match
+  if (isValid && passwordsMatch) {
+    // Style main message for success
+    message.textContent = 'Successfully Registered!';
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
 }
 
 function processFormData(e) {
   e.preventDefault();
   // Validate Form
   validateForm();
+  // Submit Form if Valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
 }
 
 // Event Listener
