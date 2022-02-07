a76923580a7f0f85f2a58f45b8592ebcf834631e
rhinoxD <coolshiv0721@gmail.com>
2022-02-07 11:24:46 +0530

    Formatted

--- math-sprint-game/script.js
+++ math-sprint-game/script.js
@@ -35,13 +35,13 @@ const wrongFormat = [];
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  // const correctEquations =
   // Set amount of wrong equations
-  // const wrongEquations = 
+  // const wrongEquations =
   // Loop through, multiply random numbers up to 9, push to array
   // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
   //   equationObject = { value: equation, evaluated: 'true' };
@@ -49,13 +49,13 @@ function createEquations() {
   // }
   // Loop through, mess with the equation results, push to array
   // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
   //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
   //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
+  //   const formatChoice =
   //   const equation = wrongFormat[formatChoice];
   //   equationObject = { value: equation, evaluated: 'false' };
   //   equationsArray.push(equationObject);
