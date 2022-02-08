0905c4dc58bdbed659e7f6b88ac1181d5c5a0510
rhinoxD <coolshiv0721@gmail.com>
2022-02-08 10:17:35 +0530

    Check Score with Timing Events

--- math-sprint-game/script.js
+++ math-sprint-game/script.js
@@ -29,8 +29,58 @@ let secondNumber = 0;
 let equationObject = {};
 const wrongFormat = [];
 
+// Time
+let timer;
+let timePlayed = 0;
+let baseTime = 0;
+let penaltyTime = 0;
+let finalTime = 0;
+let finalTimeDisplay = '0.0s';
+
 // Scroll
 let valueY = 0;
+
+// Stop Timer, Process Results, go to Score Page
+function checkTime() {
+  console.log(timePlayed);
+  if (playerGuessArray.length == questionAmount) {
+    clearInterval(timer);
+    // Check for wrong guess, add penaltyTime
+    equationsArray.forEach((equation, index) => {
+      if (equation.evaluated === playerGuessArray[index]) {
+        // Correct Guess, No Penalty
+      } else {
+        // Incorrect Guess, Add Penalty
+        penaltyTime += 0.5;
+      }
+    });
+    finalTime = timePlayed + penaltyTime;
+    console.log(
+      'time:',
+      timePlayed,
+      'penalty:',
+      penaltyTime,
+      'final:',
+      finalTime
+    );
+  }
+}
+
+// Add a tenth of a second to timePlayed
+function addTime() {
+  timePlayed += 0.1;
+  checkTime();
+}
+
+// Start timer when game page is clicked
+function startTimer() {
+  // Reset times
+  timePlayed = 0;
+  penaltyTime = 0;
+  finalTime = 0;
+  timer = setInterval(addTime, 100);
+  gamePage.removeEventListener('click', startTimer);
+}
 
 // Scroll, Store user selection in playerGuessArray
 function select(guessedTrue) {
