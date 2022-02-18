5351cbe5382482b90cd39c8a6b91498173f59e3d
rhinoxD <coolshiv0721@gmail.com>
2022-02-18 09:41:16 +0530

    Delete Item

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -55,17 +55,23 @@ function updateSavedColumns() {
   });
 }
 
+// Filter Array to remove empty values
+function filterArray(array) {
+  const filteredArray = array.filter((item) => item !== null);
+  return filteredArray;
+}
+
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
   // List Item
   const listEl = document.createElement('li');
   listEl.textContent = item;
-  // listEl.id = index;
+  listEl.id = index;
   listEl.classList.add('drag-item');
   listEl.draggable = true;
-  // listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`);
+  listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`);
   listEl.setAttribute('ondragstart', 'drag(event)');
-  // listEl.contentEditable = true;
+  listEl.contentEditable = true;
   // Append
   columnEl.appendChild(listEl);
 }
@@ -81,24 +87,38 @@ function updateDOM() {
   backlogListArray.forEach((backlogItem, index) => {
     createItemEl(backlogListEl, 0, backlogItem, index);
   });
+  backlogListArray = filterArray(backlogListArray);
   // Progress Column
   progressListEl.textContent = '';
   progressListArray.forEach((progressItem, index) => {
     createItemEl(progressListEl, 1, progressItem, index);
   });
+  progressListArray = filterArray(progressListArray);
   // Complete Column
   completeListEl.textContent = '';
   completeListArray.forEach((completeItem, index) => {
     createItemEl(completeListEl, 2, completeItem, index);
   });
+  completeListArray = filterArray(completeListArray);
   // On Hold Column
   onHoldListEl.textContent = '';
   onHoldListArray.forEach((onHoldItem, index) => {
     createItemEl(onHoldListEl, 3, onHoldItem, index);
   });
+  onHoldListArray = filterArray(onHoldListArray);
   // Don't run more than once, Update Local Storage
   updatedOnLoad = true;
   updateSavedColumns();
+}
+
+// Update Item - Delete if necessary, or update Array value
+function updateItem(id, column) {
+  const selectedArray = listArrays[column];
+  const selectedColumn = listColumns[column].children;
+  if (!selectedColumn[id].textContent) {
+    delete selectedArray[id];
+  }
+  updateDOM();
 }
 
 // Add to Column List, Reset Textbox
